dd49c7817a7f97c65b603476ae467e8b1ce16e52
bouslah celia <celiabouslah5@gmail.com>
2026-08-02 00:16:00 -0400

    Recreation de la classe Athlete

---
+++ src/model/Athlete.java
@@ -0,0 +1,72 @@
+package model;
+
+import exception.DonneesInvalidesException;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public abstract class Athlete implements PerformanceEvaluable, Comparable<Athlete> {
+
+    private String nom;
+
+    private String discipline;
+
+    protected List<Double> performances;
+
+    public Athlete(String nom, String discipline) {
+
+        this.nom = nom;
+
+        this.discipline = discipline;
+
+        this.performances = new ArrayList<>();
+
+    }
+
+    public void ajouterPerformance(double perf) throws DonneesInvalidesException {
+
+        if (perf <= 0) {
+
+            throw new DonneesInvalidesException("La performance doit être strictement positive (" + perf + " reçue pour " + nom + ").");
+
+        }
+
+        performances.add(perf);
+
+    }
+
+    public String getNom() { return nom; }
+
+    public String getDiscipline() { return discipline; }
+
+    public List<Double> getPerformances() { return performances; }
+
+    @Override
+
+    public double getMoyennePerformances() {
+
+        if (performances.isEmpty()) return 0.0;
+
+        double somme = 0;
+
+        for (double p : performances) {
+
+            somme += p;
+
+        }
+
+        return somme / performances.size();
+
+    }
+
+    @Override
+
+    public int compareTo(Athlete autre) {
+
+        return Double.compare(autre.getMeilleurePerformance(), this.getMeilleurePerformance());
+
+    }
+
+}
+
